c0d09e03b4daa867edf6083924281b67a3a2a7d4
Alexander Merritt <merritt.alex@gmail.com>
2015-06-05 14:05:34 -0700

    int to long, LIKWID markup, use mmap, extra args

--- gups_vanilla.c
+++ gups_vanilla.c
@@ -11,9 +11,15 @@
    compile with -DCHECK to check if table updates happen on correct proc */
 
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <mpich/mpi.h>
+#include <mpi.h>
+#include <likwid.h>
 #include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 
@@ -38,37 +44,52 @@ typedef unsigned long long u64Int;
 #endif
 
 u64Int HPCC_starts(s64Int n);
+size_t ns;
 
+  struct timespec t1, t2;
 int main(int narg, char **arg)
 {
   int me,nprocs;
-  int i,j,iterate,niterate;
-  int nlocal,nlocalm1,logtable,index,logtablelocal;
+  long int i,j,iterate,niterate;
+  long int nlocal,nlocalm1,logtable,index,logtablelocal;
   int logprocs,ipartner,ndata,nsend,nkeep,nrecv,maxndata,maxnfinal,nexcess;
-  int nbad,chunk,chunkbig;
+  long int nbad,chunk,chunkbig;
   double t0,t0_all,Gups;
   u64Int *table,*data,*send;
   u64Int ran,datum,procmask,nglobal,offset,nupdates;
   u64Int ilong,nexcess_long,nbad_long;
+  int mmap_populate = 0, mmap_locked = 0;
   MPI_Status status;
 
+  LIKWID_MARKER_INIT;
+  LIKWID_MARKER_THREADINIT;
+
+  int stdout_, stderr_, null = open("/dev/null", O_APPEND);
+  stdout_ = dup(1);
+  stderr_ = dup(2);
+  dup2(null, 1);
+  dup2(null, 2);
   MPI_Init(&narg,&arg);
   MPI_Comm_rank(MPI_COMM_WORLD,&me);
   MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
+  dup2(stdout_, 1);
+  dup2(stderr_, 2);
 
   /* command line args = N M chunk
      N = length of global table is 2^N
      M = # of update sets per proc
      chunk = # of updates in one set */
 
-  if (narg != 4) {
-    if (me == 0) printf("Syntax: gups N M chunk\n");
+  if (narg != 6) {
+    if (me == 0) printf("Syntax: gups N M chunk populate lock\n");
     MPI_Abort(MPI_COMM_WORLD,1);
   }
 
-  logtable = atoi(arg[1]);
-  niterate = atoi(arg[2]);
-  chunk = atoi(arg[3]);
+  logtable = strtol(arg[1], NULL, 10);
+  niterate = strtol(arg[2], NULL, 10);
+  chunk = strtol(arg[3], NULL, 10);
+  mmap_populate = (atoi(arg[4]) == 1);
+  mmap_locked   = (atoi(arg[5]) == 1);
 
   /* insure Nprocs is power of 2 */
 
@@ -99,13 +120,14 @@ int main(int narg, char **arg)
 
   chunkbig = 16*chunk;
 
-#if 0
-  table = (u64Int *) malloc(nlocal*sizeof(u64Int));
-  data = (u64Int *) malloc(chunkbig*sizeof(u64Int));
-  send = (u64Int *) malloc(chunkbig*sizeof(u64Int));
-#else
-  unsigned int flags = MAP_ANON | MAP_PRIVATE | MAP_LOCKED;
-  // flags |= MAP_POPULATE;
+  clock_gettime(CLOCK_REALTIME, &t1);
+  unsigned int flags = MAP_ANONYMOUS | MAP_PRIVATE;
+  LIKWID_MARKER_START("region_alloc");
+  if (mmap_locked)
+    flags |= MAP_LOCKED;
+  if (mmap_populate)
+    flags |= MAP_POPULATE;
+  printf("mmap %6.4f MB\n", (nlocal*8.)/1024./1024.);
   table = (u64Int *) mmap(NULL, nlocal*sizeof(u64Int),
           PROT_READ|PROT_WRITE, flags, -1, 0);
   if (MAP_FAILED == table) { perror("mmap"); return 1; }
@@ -115,7 +137,11 @@ int main(int narg, char **arg)
   send = (u64Int *) mmap(NULL, chunkbig*sizeof(u64Int),
           PROT_READ|PROT_WRITE, flags, -1, 0);
   if (MAP_FAILED == send) { perror("mmap"); return 1; }
-#endif
+  LIKWID_MARKER_STOP("region_alloc");
+  clock_gettime(CLOCK_REALTIME, &t2);
+  ns = (t2.tv_sec * 1e9 + t2.tv_nsec)
+    - (t1.tv_sec * 1e9 + t1.tv_nsec);
+  printf("alloc: %f usec\n", ns/1e3);
 
   if (!table || !data || !send) {
     if (me == 0) printf("Table allocation failed\n");
@@ -125,12 +151,19 @@ int main(int narg, char **arg)
   /* initialize my portion of global array
      global array starts with table[i] = i */
 
+  clock_gettime(CLOCK_REALTIME, &t1);
+  LIKWID_MARKER_START("region_init");
   for (i = 0; i < nlocal; i++) table[i] = i + offset;
 
   /* start my random # partway thru global stream */
 
   nupdates = (u64Int) nprocs * chunk * niterate;
   ran = HPCC_starts(nupdates/nprocs*me);
+  LIKWID_MARKER_STOP("region_init");
+  clock_gettime(CLOCK_REALTIME, &t2);
+  ns = (t2.tv_sec * 1e9 + t2.tv_nsec)
+    - (t1.tv_sec * 1e9 + t1.tv_nsec);
+  printf("init: %f usec\n", ns/1e3);
 
   /* loop:
        generate chunk random values per proc
@@ -142,6 +175,8 @@ int main(int narg, char **arg)
   nexcess = 0;
   nbad = 0;
 
+  clock_gettime(CLOCK_REALTIME, &t1);
+  LIKWID_MARKER_START("region_exec");
   MPI_Barrier(MPI_COMM_WORLD);
   t0 = -MPI_Wtime();
 
@@ -192,9 +227,16 @@ int main(int narg, char **arg)
 
   MPI_Barrier(MPI_COMM_WORLD);
   t0 += MPI_Wtime();
+  LIKWID_MARKER_STOP("region_exec");
+  clock_gettime(CLOCK_REALTIME, &t2);
+  ns = (t2.tv_sec * 1e9 + t2.tv_nsec)
+    - (t1.tv_sec * 1e9 + t1.tv_nsec);
+  printf("exec: %f usec\n", ns/1e3);
 
   /* stats */
 
+  clock_gettime(CLOCK_REALTIME, &t1);
+  LIKWID_MARKER_START("region_reduce");
   MPI_Allreduce(&t0,&t0_all,1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
   t0 = t0_all/nprocs;
 
@@ -209,6 +251,11 @@ int main(int narg, char **arg)
 
   nupdates = (u64Int) niterate * nprocs * chunk;
   Gups = nupdates / t0 / 1.0e9;
+  LIKWID_MARKER_STOP("region_reduce");
+  clock_gettime(CLOCK_REALTIME, &t2);
+  ns = (t2.tv_sec * 1e9 + t2.tv_nsec)
+    - (t1.tv_sec * 1e9 + t1.tv_nsec);
+  printf("stats: %f usec\n", ns/1e3);
 
   if (me == 0) {
     printf("Number of procs: %d\n",nprocs);
@@ -224,9 +271,40 @@ int main(int narg, char **arg)
 
   /* clean up */
 
-  free(table);
-  free(data);
-  free(send);
+#if 0
+#if defined(LIKWID_PERFMON)
+  fprintf(stderr, "# args ");
+  for (int i = 0; i < narg; i++)
+    fprintf(stderr, " %s", arg[i]);
+  fprintf(stderr, "\n");
+
+  int nevents = 10;
+  const char *regNames[] = { "region_alloc", "region_init", "region_exec", "region_stat" };
+  const char *eventNames[10];
+  likwid_getEventNames(eventNames, 10, &nevents);
+
+  fprintf(stderr, "region tsc");
+  for (int r = 0; r< nevents; r++)
+    fprintf(stderr, " %s", eventNames[r]);
+  fprintf(stderr, "\n");
+
+  nevents = 10;
+  int count;
+  double events[10], time;
+  const char *names[10];
+
+  for (int r = 0; r< 4; r++) {
+    likwid_markerGetRegion2(regNames[r], &nevents, events, names, &time, &count);
+    fprintf(stderr, "%s %lf", regNames[r], time);
+    for (int i = 0; i < nevents; i++) {
+      fprintf(stderr," %.0lf", events[i]);
+    }
+    fprintf(stderr, "\n");
+  }
+#endif
+#endif
+
+  LIKWID_MARKER_CLOSE;
   MPI_Finalize();
 }
 
